22543d813cd79228ec2b6b06796c20943d5872e8
13thUnknown <misha34500@gmail.com>
2019-12-15 23:31:36 +0300

    Some new methods

--- src/main/java/core/Host.java
+++ src/main/java/core/Host.java
@@ -1,5 +1,7 @@
 package core;
 
+import org.telegram.telegrambots.meta.api.objects.Chat;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,6 +49,13 @@ public class Host {
         return -1;
     }
 
+    private String findInHosts(long ChatID){
+        for(EntityArray host: hosts)
+            if (host.find(ChatID)!=-1)
+                return host.getName();
+        return null;
+    }
+
     private EntityArray findHost(String hostName) {
         for (EntityArray host : hosts) {
             if (host.getName().equals(hostName)) {
@@ -73,6 +82,8 @@ public class Host {
             return STATUS.NOT_EXIST;
     }
 
+
+
     public List<String> getNameList(STATUS status) {
         List<String> stringList = new ArrayList<String>();
         for (EntityArray host : hosts) {
@@ -82,10 +93,10 @@ public class Host {
         return stringList;
     }
 
-    public int addInHost(String hostName, long ChatID) {
+    public int addInHost(String hostName, long ChatID,String name) {
         EntityArray host = findHost(hostName);
         if (host != null) {
-            return host.insert(ChatID);
+            return host.insert(ChatID,name);
         } else return -2;
     }
 
@@ -96,10 +107,48 @@ public class Host {
         } else return -2;
     }
 
-    public List<String> queueInHost(String hostName) {
+    public int removeFromHost(long ChatID) {
+        EntityArray host = findHost(this.findInHosts(ChatID));
+        if (host != null) {
+            return host.delete(ChatID);
+        } else return -2;
+    }
+
+    public int removeFromHost(String hostName,int index) {
         EntityArray host = findHost(hostName);
         if (host != null) {
-            return host.arrayToString();
+            return host.delete(index);
+        } else return -2;
+    }
+
+    public int entityStatus(long ChatID){
+        EntityArray host = findHost(this.findInHosts(ChatID));
+        if (host != null) {
+            return host.entityStatus(ChatID);
+        } else return -2;
+    }
+
+    public String hostStatus(String hostName){
+        EntityArray host = findHost(hostName);
+        String status;
+        switch (host.getStatus()){
+            case OPEN:
+                status="OPEN";
+                break;
+            case CLOSE:
+                status="CLOSE";
+                break;
+            default:
+                status="INVALID";
+                break;
+        }
+        return "Resource: "+hostName+ "Status: "+status+" Number of entities: "+host.entiryCount();
+    }
+
+    public List<String> queueInHost(String hostName, PERMISSION permission) {
+        EntityArray host = findHost(hostName);
+        if (host != null) {
+            return host.arrayToString(permission);
         } else return null;
     }
 
